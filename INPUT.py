35a380b4379e3119a803b88589624f4221174072
Adam <adam.n.peace@gmail.com>
2019-04-05 18:10:55 +0000

    Add new content changes

--- INPUT.py
+++ INPUT.py
@@ -1,11 +1,20 @@
-from __future__ import absolute_import, division, print_function
-import pathlib
-import pandas as pd
-import seaborn as sns
+# Courtesy of Tensorflow
 import tensorflow as tf
+mnist = tf.keras.datasets.mnist
 
-from tensorflow import keras
-from tensorflow.keras import layers
-dataset_path = keras.utils.get_file("auto-mpg.data", "https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-dataset_path
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=1)
+model.evaluate(x_test, y_test)
